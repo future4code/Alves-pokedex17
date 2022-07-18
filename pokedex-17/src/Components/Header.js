ff0c95a9b47ff8fcecd31ee21d457f4e278d41fe
Ari Himmelstein <ahimmelstein@gmail.com>
2022-07-18 18:44:45 -0300

    Theme e Header preliminar

--- pokedex-17/src/Components/Header.js
+++ pokedex-17/src/Components/Header.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import { AppBar, Button, Grid } from '@mui/material'
+import { Toolbar } from '@mui/material'
+import { Typography } from '@mui/material'
+import { Box } from '@mui/system'
+
+
 
 function Header() {
   return (
     <div>
-       <h1>Header do Pokedex17</h1> 
+        <AppBar position='fixed' color='primary'>
+            <Toolbar>
+                <Grid container>
+                    <Grid item xs={2}>
+                        <Typography variant="h4" color='secondary'>
+                            PokedeX 
+                        </Typography>
+                    </Grid>
+                    <Grid item  xs={6}>
+                    </Grid>
+                         <Grid item xs={1}/>
+                         <Grid item xs={3}>
+                            <Box display="flex">
+                                <Button sx={{marginLeft: 'auto'}} variant="contained">Detalhes</Button>
+                                <Button sx={{marginLeft: 2 }}variant="contained">Pokedex</Button>
+                            </Box>
+                         </Grid>
+                                       
+                
+                </Grid>
+                                   
+            </Toolbar>
+        </AppBar>
     </div>
+
   )
 }
 
