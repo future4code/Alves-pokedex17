fee4d181048141b780469529408f430e4bf9f1e1
Ari Himmelstein <ahimmelstein@gmail.com>
2022-07-21 19:33:57 -0300

    Logica para adicionar na pokedex e ajustes header e App

--- pokedex-17/src/App.js
+++ pokedex-17/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import Header from "./Components/Header";
-import Footer from "./Components/Footer";
 import Router from "./Components/Router";
 
 function App() {
   return (
     <div>
       <Header/>
-      <h1>Pokedex 17</h1>
+      
       <Router/>
-      <Footer/>
+      
     </div>
   );
 }
--- pokedex-17/src/Pages/Home.js
+++ pokedex-17/src/Pages/Home.js
@@ -50,17 +50,10 @@ export default function Home() {
     <div>
  
         <Header />
-          {/* <Box m={2}>
-            <Grid
-              container
-              spacing={{ xs: 2, md: 3 }}
-              columns={{ xs: 4, sm: 2, md: 12 }}
-            >
-              {renderPokemonList}
-
-            </Grid> 
-          </Box>  */}
-        <PokemonCard />
+           <Box m={9}>
+           	 <PokemonCard />
+           </Box>  
+        
 
     </div>
   
